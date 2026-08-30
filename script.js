1aa884b3e1117033f064c4c1d6ba6029df65c027
usausamikann <orangeusag0611@gmail.com>
2026-08-30 22:26:36 +0900

    ・タイマーの開始挙動を変更

--- script.js
+++ script.js
@@ -4,25 +4,29 @@ const timerText = document.getElementById("timer");
 
 let score = 0;
 let timeLeft = 15;
+let started = false;
 
 //クリックごとにscore++
 target.addEventListener("click", () => {
     score++;
-    scoreText.textContent = `Score: ${score}`;
-});
+    scoreText.textContent = `Score: ${score}`; //クリックごとにスコア更新
+    //初回クリック後にゲーム開始
+    if(!started){
+        started=true;
+        // 1秒ごとに残り時間を1減らす
+        const timerId = setInterval(() => {
+            timeLeft--;
+            timerText.textContent = `Time: ${timeLeft}`;
 
-// 1秒ごとに残り時間を1減らす
-const timerId = setInterval(() => {
-    timeLeft--;
-    timerText.textContent = `Time: ${timeLeft}`;
-
-    if (timeLeft <= 0) { //15sec経過後
-        clearInterval(timerId);
-        target.disabled = true; //ボタン無効化
-        //リザルト表示
-        timerText.style.display = "none";
-        target.style.display = "none";
-        scoreText.style.fontSize = "64px";
-        scoreText.textContent = `Score: ${score}`;
+            if (timeLeft <= 0) { //15sec経過後
+                clearInterval(timerId);
+                target.disabled = true; //ボタン無効化
+                //リザルト表示
+                timerText.style.display = "none";
+                target.style.display = "none";
+                scoreText.style.fontSize = "64px";
+                scoreText.textContent = `Score: ${score}`;
+            }
+        }, 1000); //1000ms=1secごとに実行
     }
-}, 1000); //1000ms=1secごとに実行
+});
