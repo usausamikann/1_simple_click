759adac01a9052ecb09a24c5765629b4e893c2f5
usausamikann <orangeusag0611@gmail.com>
2026-08-30 22:08:37 +0900

    タイマー設定を実装

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const target = document.getElementById("target");
 const scoreText = document.getElementById("score");
+const timerText = document.getElementById("timer");
 
 let score = 0;
+let timeLeft = 20;
 
 //クリックごとにscore++
 target.addEventListener("click", () => {
@@ -9,3 +11,13 @@ target.addEventListener("click", () => {
     scoreText.textContent = `Score: ${score}`;
 });
 
+// 1秒ごとに残り時間を1減らす
+const timerId = setInterval(() => {
+    timeLeft--;
+    timerText.textContent = `Time: ${timeLeft}`;
+
+    if (timeLeft <= 0) {
+        clearInterval(timerId);
+        target.disabled = true; //ボタン無効化
+    }
+}, 1000); //1000ms=1sごとに実行
